8ad0222863f111ee6a826e37342dfcf6386ecbe4
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-03-19 16:02:14 +0800

    Updates

--- src/components/Event_Review/Review2023.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Review2023 = () => {
-  return (
-    <div>Review2023</div>
-  )
-}
-
-export default Review2023
--- src/components/Event_Review/Review2024.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Review2024 = () => {
-  return (
-    <div>Review2024</div>
-  )
-}
-
-export default Review2024
